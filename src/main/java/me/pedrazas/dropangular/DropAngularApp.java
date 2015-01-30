7b033654a1c9f29931b3aa4d881663f7b2ed9f70
Ivan Pedrazas <ipedrazas@gmail.com>
2015-01-30 17:55:06 +0000

    Added CORS support

--- src/main/java/me/pedrazas/dropangular/DropAngularApp.java
+++ src/main/java/me/pedrazas/dropangular/DropAngularApp.java
@@ -11,6 +11,10 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import me.pedrazas.dropangular.om.Ping;
 
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration.Dynamic;
+
 public class DropAngularApp extends Application<DropAngularConfiguration> {
 	public static void main(String[] args) throws Exception {
 		new DropAngularApp().run(args);
@@ -53,5 +57,18 @@ public class DropAngularApp extends Application<DropAngularConfiguration> {
     	((DefaultServerFactory) config.getServerFactory()).setJerseyRootPath("/api/*");
     	
     	environment.healthChecks().register("Ping", new PingHealthCheck());
-	}
+	    	// CORS
+    	configureCors(environment);
+    }
+    
+    private void configureCors(Environment environment) {
+        Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowCredentials", "true");
+      }
+
 }
